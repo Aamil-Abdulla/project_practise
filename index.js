5b374afa82b756bb635ae8e6bdb7dd1dfb153db2
Aamil Abdulla <aamilabdulla0@gmail.com>
2024-12-11 15:34:39 +0530

    assignments

---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let ADMINS = [];
+let USERS = [];
+let COURSES = [];
+
+function adminAuthentication(req,res,next){
+  const admin = ADMINS.find(a=>a.username === username && a.password === password )
+  if (admin){
+    next()
+  }
+  else{
+    res.status(403).json({message: 'Admin aunthentication failed' })
+  }
+}
+
+function userAuthentication(req,res,next){
+  const user = USERS.find(u=>u.username === username && u.password === password)
+  const {username ,password} = req.headers
+  if (user) {
+    req.user = user
+    next()
+  }
+  else{res.status(200).json({message:'User authentication is invalid'})}
+}
+// Admin routes
+app.post('/admin/signup', (req, res) => {
+
+  const admin = req.body
+  const existingAdmin = ADMINS.find(a=>a.username === admin.username)
+  
+  if (existingAdmin){
+    res.status(403).json({message : 'The admin is already signed in'})
+  }
+  else{
+    ADMINS.push(admin)
+    res.json(201).json({message : 'it is successfully created'})
+  }
+});
+
+app.post('/admin/login', adminAuthentication, (req, res) => {
+  res.json({message  : " Logged in Succesfully"})
+});
+
+app.post('/admin/courses', adminAuthentication, (req, res) => {
+  const course = req.body;
+  course.id = Date.now()
+  if (!course.title){
+    res.status(411).json({message : "please give the title"})
+  }
+    COURSES.push(course)
+    res.status(201).json({message : 'the course is succesfully created', courseId : course.id })
+  }
+);
+
+app.put('/admin/courses/:courseId',adminAuthentication,(req, res) => {
+  const courseId = parseInt(req.params.courseId)
+  const course =  COURSES.find(c=>c.courseId === courseId)
+  if (course) {
+    Object.assign(course,req.body)
+    res.json({message: "Course updated Succesfuly" })    
+  }
+  else{
+    res.status(403).json("Course not found")
+  }
+});
+
+app.get('/admin/courses',adminAuthentication, (req, res) => {
+  res.json({courses : COURSES})
+});
+
+// User routes
+app.post('/users/signup', (req, res) => {
+  const userExist = USERS.find(u=>u.username === username && u.password === password)
+  if (userExist){
+    res.status(403).json({message : "User already Signed up"})
+  }
+  else{
+    const user = {... req.body,purchasedCourses : []}
+    USERS.push(user)
+    res.status(201).json({message : "User succesfully Signed up"})
+
+  }
+});
+
+app.post('/users/login',userAuthentication ,(req, res) => {
+  const user = req.body
+  res.status(200).json({message : "User logged in succesfully"})
+
+}); 
+
+app.get('/users/courses',userAuthentication, (req, res) => {
+  COURSES.filter(c=> c.published)
+  res.json({course : COURSES})
+});
+
+app.post('/users/courses/:courseId',userAuthentication, (req, res) => {
+  const alreadypurchasedCourse = COURSES.find(c=>c.courseId === courseId)
+  if (alreadypurchasedCourse){
+    res.status(403).json({message : "THE Course is already purchased"})
+  } 
+  COURSES.push()
+});
+
+app.get('/users/purchasedCourses', (req, res) => {
+  // logic to view purchased courses
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
